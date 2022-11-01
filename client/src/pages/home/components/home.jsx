afd572bcb74eb08638d32ab8b5012dd7594b1e7c
JeffeVargasP <jeffevargas@proton.me>
2022-11-01 17:43:11 -0300

    PRE FAVORE GIOVANNE CORRIJAME

--- client/src/pages/home/components/home.jsx
+++ client/src/pages/home/components/home.jsx
@@ -1,23 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 
 import '../../home/styles/main.css';
+import start from '../../../index.js';
 
 export default function RegisterComponent() {
     const navigate = useNavigate();
 
     return (
-        <>
             <div className='container'>
                 <div className='content'>
                     <div className='title'>
-                        <h1 className='title_animation'>Fatec</h1>
-                        <h1 className='title_animation'>Projeto Integrador</h1>
+                        <h1 className='title_animation' id="output"></h1>
+                        <h1 className='title_animation' id="assistent">Projeto Integrador</h1>
                     </div>
-                    <button>
+                    <button id="start" onClick={start}>
                         <div className='border_button'></div>
                     </button>
                 </div>
             </div>
-        </>
     );
 }
